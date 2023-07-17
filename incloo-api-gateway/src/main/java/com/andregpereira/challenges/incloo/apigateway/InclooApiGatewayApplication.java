7da7b48f65d7ab7bebd9322b8cdb878c95157a7c
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-07-16 23:59:34 -0300

    Habilitar Discovery Client

--- incloo-api-gateway/src/main/java/com/andregpereira/challenges/incloo/apigateway/InclooApiGatewayApplication.java
+++ incloo-api-gateway/src/main/java/com/andregpereira/challenges/incloo/apigateway/InclooApiGatewayApplication.java
@@ -2,12 +2,12 @@ package com.andregpereira.challenges.incloo.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InclooApiGatewayApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InclooApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InclooApiGatewayApplication.class, args);
+    }
 }
