498bda74c04275e0e6ff53e9aba7833ca2969eb8
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-07-16 23:59:42 -0300

    Criar controller com método fallback

---
+++ incloo-api-gateway/src/main/java/com/andregpereira/challenges/incloo/apigateway/app/rest/GatewayController.java
@@ -0,0 +1,21 @@
+package com.andregpereira.challenges.incloo.apigateway.app.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequestMapping("/fallback")
+public class GatewayController {
+
+    @RequestMapping
+    public Mono<ResponseEntity<Object>> fallback() {
+        log.error("Erro ao acessar a API");
+        return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(
+                "URL indisponível no momento. Tente novamente"));
+    }
+}
