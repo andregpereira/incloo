7dded672b5cb92e645938dc93b885ecf61700e9d
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-04 17:34:14 -0300

    Criar mapper de service

---
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/mapper/UsuarioServiceMapper.java
@@ -0,0 +1,20 @@
+package com.andregpereira.challenges.incloo.inclooapi.app.mapper;
+
+import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioCreateDto;
+import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioDto;
+import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioUpdateDto;
+import com.andregpereira.challenges.incloo.inclooapi.domain.model.Usuario;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UsuarioServiceMapper {
+
+    Usuario toUsuario(UsuarioCreateDto dto);
+
+    Usuario toUsuario(UsuarioUpdateDto dto);
+
+    UsuarioDto toUsuarioDto(Usuario u);
+
+}
