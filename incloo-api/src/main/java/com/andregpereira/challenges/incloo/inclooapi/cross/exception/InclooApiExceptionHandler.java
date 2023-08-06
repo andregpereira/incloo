8db3bd9fcdb52ad077d0787889189be5b1788847
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-06 18:40:49 -0300

    Criar record com campo e mensagem e tratar exceção de campos inválidos

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/cross/exception/InclooApiExceptionHandler.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/cross/exception/InclooApiExceptionHandler.java
@@ -3,8 +3,11 @@ package com.andregpereira.challenges.incloo.inclooapi.cross.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -13,13 +16,32 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class InclooApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private static final String LOG_TRATANDO_EXCECAO = "Tratando exceção {}";
+    private static final String LOG_TRATANDO_EXCECAO = "Tratando exceção [{}]";
 
     @Override
     protected ResponseEntity<Object> handleErrorResponseException(ErrorResponseException ex, HttpHeaders headers,
             HttpStatusCode status, WebRequest request) {
         log.warn(LOG_TRATANDO_EXCECAO, ex.toString());
         return super.handleErrorResponseException(ex, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn(LOG_TRATANDO_EXCECAO, ex.toString());
+        ProblemDetail body = ex.getBody();
+        body.setTitle("Campos inválidos");
+        body.setProperty("errors", ex.getFieldErrors().stream().map(DadoInvalido::new));
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
+
+    private record DadoInvalido(String campo,
+            String msg) {
+
+        public DadoInvalido(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+
     }
 
 }
