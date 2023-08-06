24516f6263e4134f150006678df0eb2b4b9219ec
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-06 18:19:48 -0300

    Renomear parâmetro

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/domain/usecase/usuario/UsuarioUpdateUc.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/domain/usecase/usuario/UsuarioUpdateUc.java
@@ -4,6 +4,6 @@ import com.andregpereira.challenges.incloo.inclooapi.domain.model.Usuario;
 
 public sealed interface UsuarioUpdateUc permits UsuarioUpdateUcImpl {
 
-    Usuario update(Usuario usuario);
+    Usuario update(Long id, Usuario usuarioAtualizado);
 
 }
