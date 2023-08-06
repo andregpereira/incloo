d5ccafaa14f2b20c84734cd60ab17ab277b070fa
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-06 18:20:20 -0300

    Add regras de negócio e atualizar atributos

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/domain/usecase/usuario/UsuarioUpdateUcImpl.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/domain/usecase/usuario/UsuarioUpdateUcImpl.java
@@ -1,5 +1,6 @@
 package com.andregpereira.challenges.incloo.inclooapi.domain.usecase.usuario;
 
+import com.andregpereira.challenges.incloo.inclooapi.cross.exception.UsuarioAlreadyExistsException;
 import com.andregpereira.challenges.incloo.inclooapi.domain.gateway.UsuarioGateway;
 import com.andregpereira.challenges.incloo.inclooapi.domain.model.Usuario;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,14 @@ public non-sealed class UsuarioUpdateUcImpl implements UsuarioUpdateUc {
     private final UsuarioGateway gateway;
 
     @Override
-    public Usuario update(Usuario usuario) {
+    public Usuario update(Long id, Usuario usuarioAtualizado) {
+        if (gateway.existsByEmail(usuarioAtualizado.getEmail()))
+            throw new UsuarioAlreadyExistsException("e-mail");
+        Usuario usuario = gateway.findByIdAndAtivoTrue(id);
+        usuario.setNome(usuarioAtualizado.getNome());
+        usuario.setSobrenome(usuarioAtualizado.getSobrenome());
+        usuario.setEmail(usuarioAtualizado.getEmail());
+        usuario.setCelular(usuarioAtualizado.getCelular());
         return gateway.save(usuario);
     }
 
