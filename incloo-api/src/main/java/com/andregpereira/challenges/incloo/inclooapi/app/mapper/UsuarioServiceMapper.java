93a8fa13251b5d66e3be4ec9d81ad9acb11f03b3
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-06 18:30:34 -0300

    Setar ativo true para cadastro e atualização

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/mapper/UsuarioServiceMapper.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/mapper/UsuarioServiceMapper.java
@@ -5,14 +5,19 @@ import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioDto;
 import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioUpdateDto;
 import com.andregpereira.challenges.incloo.inclooapi.domain.model.Usuario;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.NullValueMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface UsuarioServiceMapper {
 
+    @Mapping(target = "ativo", expression = "java(true)")
     Usuario toUsuario(UsuarioCreateDto dto);
 
+    @Mapping(target = "ativo", expression = "java(true)")
     Usuario toUsuario(UsuarioUpdateDto dto);
 
     UsuarioDto toUsuarioDto(Usuario u);
