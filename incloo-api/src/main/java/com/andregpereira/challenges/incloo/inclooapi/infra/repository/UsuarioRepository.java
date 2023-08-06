a92a116b9189027fecf1f39b154e0f0697f988c5
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-06 18:17:02 -0300

    Add consulta por e-mail e @Query para CPF

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/infra/repository/UsuarioRepository.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/infra/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.andregpereira.challenges.incloo.inclooapi.infra.repository;
 
 import com.andregpereira.challenges.incloo.inclooapi.infra.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,12 +10,24 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
+    @Query(value = """
+            SELECT COUNT(cpf) > 0 FROM tb_usuarios u
+            WHERE u.cpf LIKE REPLACE(REPLACE(:cpf, '.', ''), '-', '')
+            """, nativeQuery = true)
     boolean existsByCpf(String cpf);
+
+    boolean existsByEmail(String email);
 
     Optional<UsuarioEntity> findByCpf(String cpf);
 
     Optional<UsuarioEntity> findByIdAndAtivoTrue(Long id);
 
     Optional<UsuarioEntity> findByIdAndAtivoFalse(Long id);
+
+    @Query(value = """
+            SELECT * FROM tb_usuarios u
+            WHERE u.cpf LIKE REPLACE(REPLACE(:cpf, '.', ''), '-', '')
+            """, nativeQuery = true)
+    Optional<UsuarioEntity> findByCpfAndAtivoTrue(String cpf);
 
 }
