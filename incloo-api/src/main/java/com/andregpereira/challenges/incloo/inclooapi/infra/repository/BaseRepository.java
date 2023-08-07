1d509f3ddf6fd4eb6869c1da64ef038a96625f09
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-07 20:39:32 -0300

    Criar repositório base

---
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/infra/repository/BaseRepository.java
@@ -0,0 +1,15 @@
+package com.andregpereira.challenges.incloo.inclooapi.infra.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface BaseRepository<T> extends JpaRepository<T, Long> {
+
+    Optional<T> findByIdAndAtivoTrue(Long id);
+
+    Optional<T> findByIdAndAtivoFalse(Long id);
+
+}
