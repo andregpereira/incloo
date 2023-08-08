94a587f2da58bd9b5161b97df2b80e0df7b6b761
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-08 20:02:16 -0300

    Estender BaseRepository, add consultas por e-mail e nome, refatorar consulta por CPF

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/infra/repository/UsuarioRepository.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/infra/repository/UsuarioRepository.java
@@ -1,14 +1,16 @@
 package com.andregpereira.challenges.incloo.inclooapi.infra.repository;
 
 import com.andregpereira.challenges.incloo.inclooapi.infra.entity.UsuarioEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+public interface UsuarioRepository extends BaseRepository<UsuarioEntity> {
 
     @Query(value = """
             SELECT COUNT(cpf) > 0 FROM tb_usuarios u
@@ -18,16 +20,18 @@ public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
     boolean existsByEmail(String email);
 
+    @Query(value = """
+            SELECT * FROM tb_usuarios u
+            WHERE u.cpf LIKE REPLACE(REPLACE(:cpf, '.', ''), '-', '') AND u.ativo = true
+            """, nativeQuery = true)
     Optional<UsuarioEntity> findByCpf(String cpf);
 
-    Optional<UsuarioEntity> findByIdAndAtivoTrue(Long id);
-
-    Optional<UsuarioEntity> findByIdAndAtivoFalse(Long id);
+    Optional<UsuarioEntity> findByEmailAndAtivoTrue(String email);
 
     @Query(value = """
             SELECT * FROM tb_usuarios u
-            WHERE u.cpf LIKE REPLACE(REPLACE(:cpf, '.', ''), '-', '')
+            WHERE u.nome ILIKE %:nome% AND u.ativo=true
             """, nativeQuery = true)
-    Optional<UsuarioEntity> findByCpfAndAtivoTrue(String cpf);
+    Page<UsuarioEntity> findByNome(@Param("nome") String nome, Pageable pageable);
 
 }
