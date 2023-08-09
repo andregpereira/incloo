0c5d080b9517b7cc3d69aa79695cd6d019926a02
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-09 20:28:39 -0300

    Estender interface genérica

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/service/usuario/UsuarioConsultaService.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/service/usuario/UsuarioConsultaService.java
@@ -1,16 +1,13 @@
 package com.andregpereira.challenges.incloo.inclooapi.app.service.usuario;
 
 import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioDto;
+import com.andregpereira.challenges.incloo.inclooapi.app.service.ConsultaService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public sealed interface UsuarioConsultaService permits UsuarioConsultaServiceImpl {
-
-    Page<UsuarioDto> findAll(Pageable pageable);
+public sealed interface UsuarioConsultaService extends ConsultaService<UsuarioDto> permits UsuarioConsultaServiceImpl {
 
     Page<UsuarioDto> findByNome(String nome, Pageable pageable);
-
-    UsuarioDto findById(Long id);
 
     UsuarioDto findByCpf(String cpf);
 
