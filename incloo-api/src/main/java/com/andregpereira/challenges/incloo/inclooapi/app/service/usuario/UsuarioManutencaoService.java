e11b5715aa95663381a451373729588d0a11fd11
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-09 20:29:02 -0300

    Estender interface genérica

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/service/usuario/UsuarioManutencaoService.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/service/usuario/UsuarioManutencaoService.java
@@ -3,15 +3,8 @@ package com.andregpereira.challenges.incloo.inclooapi.app.service.usuario;
 import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioCreateDto;
 import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioDto;
 import com.andregpereira.challenges.incloo.inclooapi.app.dto.usuario.UsuarioUpdateDto;
+import com.andregpereira.challenges.incloo.inclooapi.app.service.ManutencaoService;
 
-public sealed interface UsuarioManutencaoService permits UsuarioManutencaoServiceImpl {
-
-    UsuarioDto create(UsuarioCreateDto dto);
-
-    UsuarioDto update(Long id, UsuarioUpdateDto dto);
-
-    void activate(Long id);
-
-    void deactivate(Long id);
+public sealed interface UsuarioManutencaoService extends ManutencaoService<UsuarioCreateDto, UsuarioUpdateDto, UsuarioDto> permits UsuarioManutencaoServiceImpl {
 
 }
