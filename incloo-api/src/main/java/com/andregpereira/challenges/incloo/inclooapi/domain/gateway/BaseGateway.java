40f2a009885a85ed968e60e58b1b1b3563fc89c3
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-09 18:54:32 -0300

    Add interfaces funcionais nos métodos

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/domain/gateway/BaseGateway.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/domain/gateway/BaseGateway.java
@@ -1,13 +1,18 @@
 package com.andregpereira.challenges.incloo.inclooapi.domain.gateway;
 
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
 public interface BaseGateway<T> {
 
     T save(T t);
 
     T findById(Long id);
 
-    T findByIdAndAtivoTrue(Long id);
+    T findByIdAndAtivoTrue(Long id, UnaryOperator<T> op);
 
-    T findByIdAndAtivoFalse(Long id);
+    void findByIdAndAtivoTrue(Long id, Consumer<T> op);
+
+    void findByIdAndAtivoFalse(Long id, Consumer<T> op);
 
 }
