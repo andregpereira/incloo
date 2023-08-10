1edde7e6c276f8ef13e1fe11114c0e1e9e3d6935
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-09 21:56:06 -0300

    Criar mapper de service de vaga

---
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/mapper/VagaServiceMapper.java
@@ -0,0 +1,19 @@
+package com.andregpereira.challenges.incloo.inclooapi.app.mapper;
+
+import com.andregpereira.challenges.incloo.inclooapi.app.dto.vaga.VagaCreateDto;
+import com.andregpereira.challenges.incloo.inclooapi.app.dto.vaga.VagaDto;
+import com.andregpereira.challenges.incloo.inclooapi.domain.model.Vaga;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+public interface VagaServiceMapper {
+
+    Vaga toVaga(VagaCreateDto dto);
+
+    VagaDto toVagaDto(Vaga v);
+
+}
