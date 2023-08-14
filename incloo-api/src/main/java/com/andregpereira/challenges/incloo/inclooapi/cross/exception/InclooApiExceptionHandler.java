c2604796dd0d621e81ee02425e9d17c4859d8ca5
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-08-14 19:18:00 -0300

    Tratar mais exceções

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/cross/exception/InclooApiExceptionHandler.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/cross/exception/InclooApiExceptionHandler.java
@@ -1,16 +1,21 @@
 package com.andregpereira.challenges.incloo.inclooapi.cross.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.text.MessageFormat;
 
 @Slf4j
 @RestControllerAdvice
@@ -31,8 +36,39 @@ public class InclooApiExceptionHandler extends ResponseEntityExceptionHandler {
         log.warn(LOG_TRATANDO_EXCECAO, ex.toString());
         ProblemDetail body = ex.getBody();
         body.setTitle("Campos inválidos");
+        body.setDetail("Um ou mais campos não foram preenchidos corretamente");
         body.setProperty("errors", ex.getFieldErrors().stream().map(DadoInvalido::new));
         return super.handleExceptionInternal(ex, null, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+            HttpStatusCode status, WebRequest request) {
+        log.warn(LOG_TRATANDO_EXCECAO, ex.toString());
+        ProblemDetail body = createProblemDetail(ex, status, "Parâmetro inválido. Verifique e tente novamente", null,
+                null, request);
+        body.setTitle("Parâmetro inválido");
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn(LOG_TRATANDO_EXCECAO, ex.toString());
+        ProblemDetail body = ex.getBody();
+        body.setTitle("Parâmetro inválido");
+        body.setProperty("errors", new DadoInvalido(ex.getParameterName(),
+                MessageFormat.format("O campo {0} é obrigatório", ex.getParameterName())));
+        return super.handleExceptionInternal(ex, null, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn(LOG_TRATANDO_EXCECAO, ex.toString());
+        ProblemDetail body = super.createProblemDetail(ex, status,
+                "Informação inválida. Verifique os dados e tente novamente", null, null, request);
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 
     private record DadoInvalido(String campo,
