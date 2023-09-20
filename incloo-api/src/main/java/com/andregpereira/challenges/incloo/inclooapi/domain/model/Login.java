a673b0076c35d44ceb7bf9af9e2bcb9c9a0bd505
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-09-20 15:25:26 -0300

    Criar entidade login

---
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/domain/model/Login.java
@@ -0,0 +1,15 @@
+package com.andregpereira.challenges.incloo.inclooapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Login {
+
+    private String username;
+    private String password;
+
+}
