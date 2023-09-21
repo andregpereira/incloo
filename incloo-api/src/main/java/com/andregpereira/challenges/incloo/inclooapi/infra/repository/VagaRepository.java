e06c2c287a391c46990a2479920bc72af1449cc0
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-09-20 21:10:59 -0300

    Refatorar consulta por publico alvo

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/infra/repository/VagaRepository.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/infra/repository/VagaRepository.java
@@ -7,12 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
+import java.util.Collection;
 
 @Repository
 public interface VagaRepository extends BaseRepository<VagaEntity> {
 
-    boolean existsByTitle(String titulo);
+    boolean existsByTitle(String title);
 
     @Query(value = """
             SELECT * FROM tb_vagas v
@@ -20,6 +20,14 @@ public interface VagaRepository extends BaseRepository<VagaEntity> {
             """, nativeQuery = true)
     Page<VagaEntity> findByTitle(@Param("title") String title, Pageable pageable);
 
-    Page<VagaEntity> findByMinorityGroupsIn(Set<String> minorityGroups, Pageable pageable);
+    @Query(value = """
+            SELECT * FROM tb_vagas v WHERE v.id_vaga IN
+            (SELECT va.id_vaga FROM tb_vagas va
+            JOIN vaga_minority_groups vmg ON va.id_vaga = vmg.vaga_id_vaga WHERE vmg.minority_groups IN
+            (:minorityGroups) GROUP BY va.id_vaga
+            HAVING COUNT(va.id_vaga) = :countMinorityGroups)
+            """, nativeQuery = true)
+    Page<VagaEntity> findByMinorityGroupsContainsIn(@Param("minorityGroups") Collection<String> minorityGroups,
+            @Param("countMinorityGroups") long countMinorityGroups, Pageable pageable);
 
 }
