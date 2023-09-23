fbcc9f484d44ff93501ede0dded828b48dda5e62
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-09-23 19:14:05 -0300

    Criar mapper de service de candidatura

---
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/mapper/CandidaturaServiceMapper.java
@@ -0,0 +1,25 @@
+package com.andregpereira.challenges.incloo.inclooapi.app.mapper;
+
+import com.andregpereira.challenges.incloo.inclooapi.app.dto.candidatura.CandidaturaCreateDto;
+import com.andregpereira.challenges.incloo.inclooapi.app.dto.candidatura.CandidaturaDto;
+import com.andregpereira.challenges.incloo.inclooapi.domain.model.Candidatura;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+public interface CandidaturaServiceMapper {
+
+    @Mapping(target = "cv", expression = "java(cv.getBytes())")
+    @Mapping(target = "submittedTechnicalTest", expression = "java(technicalTest.getBytes())")
+    Candidatura toCandidatura(CandidaturaCreateDto dto, MultipartFile cv, MultipartFile technicalTest) throws IOException;
+
+    CandidaturaDto toCandidaturaDto(Candidatura v);
+
+}
