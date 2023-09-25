4a1f1b8b1a3d129ec058a2e7e7e4b836da070d37
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-09-25 15:15:43 -0300

    Refatorar mapper

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/mapper/VagaServiceMapper.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/mapper/VagaServiceMapper.java
@@ -5,15 +5,23 @@ import com.andregpereira.challenges.incloo.inclooapi.app.dto.vaga.VagaDetalhesDt
 import com.andregpereira.challenges.incloo.inclooapi.app.dto.vaga.VagaDto;
 import com.andregpereira.challenges.incloo.inclooapi.domain.model.Vaga;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.mapstruct.NullValueMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE,
         nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface VagaServiceMapper {
 
     Vaga toVaga(VagaCreateDto dto);
+
+    @Mapping(target = "technicalTest",
+            expression = "java(org.hibernate.engine.jdbc.BlobProxy.generateProxy(technicalTest.getBytes()))")
+    Vaga toVaga(VagaCreateDto dto, MultipartFile technicalTest) throws IOException;
 
     VagaDto toVagaDto(Vaga v);
 
