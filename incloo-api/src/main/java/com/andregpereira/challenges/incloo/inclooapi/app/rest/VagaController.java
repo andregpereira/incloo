e9506ffcc66bf7a359bbe36b5d4a5bfa327c1528
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-09-25 15:15:54 -0300

    Refatorar controller

--- incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/rest/VagaController.java
+++ incloo-api/src/main/java/com/andregpereira/challenges/incloo/inclooapi/app/rest/VagaController.java
@@ -8,11 +8,13 @@ import com.andregpereira.challenges.incloo.inclooapi.app.service.vaga.VagaManute
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -30,9 +32,10 @@ public class VagaController {
     private final VagaConsultaService consultaService;
 
     @PostMapping
-    public ResponseEntity<VagaDto> create(@RequestBody @Valid VagaCreateDto dto) {
+    public ResponseEntity<VagaDto> create(@RequestPart @Valid VagaCreateDto dto,
+            @RequestPart(required = false) MultipartFile technicalTest) {
         log.info("Criando vaga...");
-        VagaDto vaga = manutencaoService.create(dto);
+        VagaDto vaga = manutencaoService.create(dto, technicalTest);
         log.info("Vaga criada com sucesso");
         URI uri = UriComponentsBuilder.fromPath("/vagas/{id}").buildAndExpand(vaga.id()).toUri();
         return ResponseEntity.created(uri).body(vaga);
@@ -57,6 +60,11 @@ public class VagaController {
     public ResponseEntity<Void> deactivate(@PathVariable Long id) {
         manutencaoService.deactivate(id);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("baixar-teste/{id}")
+    public ResponseEntity<ByteArrayResource> downloadTechnicalTest(@PathVariable Long id) {
+        return ResponseEntity.ok(consultaService.downloadTechnicalTest(id));
     }
 
     @GetMapping("/{id}")
